50838f4bd8c0d0026ff060e18e2d5b1f4ee8f79e
Shivam Kotwal <kotwal.shivam1999@gmail.com>
2020-05-20 21:10:47 -0700

    this is java file

--- src/myJavaProject/myAppTester.java
+++ src/myJavaProject/myAppTester.java
@@ -3,6 +3,8 @@
  */
 package myJavaProject;
 
+import java.util.Scanner;
+
 /**
  * @author user
  *
@@ -14,6 +16,17 @@ public class myAppTester {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		System.out.println("Hello Software Engineering World");
+		
+		Scanner input = new Scanner(System.in);
+		
+		 System.out.print("Input the starting number of the range: "); 
+	        int rstartNum = input.nextInt(); 
+	        System.out.print("Input the ending number of the range: "); 
+	        int rendnum = input.nextInt(); 
+		    int random_num = rstartNum + (int)(Math.random() * ((rendnum - rstartNum) + 1));
+		    System.out.println(random_num);
 
 	}
 
